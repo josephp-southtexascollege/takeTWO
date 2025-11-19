38f7701a12bec68c653f572f966a985aa605262c
josephperez956 <josephperez@yahoo.com>
2025-11-19 03:00:04 -0600

    pagination individual buttons

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -16,13 +16,18 @@ const nextPage = () => {
           activePage.value++;
      }
 }
+
+const selectedPage = (buttonName) => {
+     //const buttonValue = event.button.value
+     activePage.value = parseInt(buttonName)
+  }
 </script>
 
 
 <template>
      <div class="flex justify-center gap-4 p-8">
           <button @click="prevPage" :disabled="activePage===1" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md">PREV</button>
-          <button v-for="page in pages" :key="page" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md" :class="page===activePage ? 'bg-green-700 text-white':''">{{ page }}</button>
+          <button @click="selectedPage(page)" v-for="page in pages" :key="page" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md" :class="page===activePage ? 'bg-green-700 text-white':''">{{ page }}</button>
           <button @click="nextPage" :disabled="activePage===pages" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md">NEXT</button>
      </div>
 </template>
