5108b560e92d1a9cdd92a2bc34ee610a4a5bd397
josephperez956 <josephperez@yahoo.com>
2025-11-18 23:20:54 -0600

    CardDetails Done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -2,18 +2,21 @@
 import { faker } from '@faker-js/faker';
 const firstname = faker.person.firstName();
 const lastname = faker.person.lastName();
+const fullname = faker.person.fullName({ firstName: firstname, lastName: lastname });
 const jobtitle = faker.person.jobTitle();
 const bio = faker.lorem.lines(2);
 const username = faker.internet.username({ firstName: firstname, lastName: lastname }).toLowerCase();
 </script>
 
 <template>
+     <RouterLink :to="{name: 'CardDetails', params: { id: username } }">
      <div class="border border-gray-400 rounded-t-xl shadow bg-gray-200 h-full">
           <img class="object-fill h-480 w-960 rounded-t-xl" v-bind:src="faker.image.urlLoremFlickr({category: 'cars', height: 480, width: 960})" />
           <div class="p-4">
-               <p class="mb-2 text-2xl font-bold ">{{ firstname }} {{ lastname  }} </p>
+               <p class="mb-2 text-2xl font-bold ">{{ fullname }} </p>
                <p class="mb-3 font-normal text-gray-700 ">{{ jobtitle }}</p>
                <p class="mb-3 font-nornaml text-gray-700 ">{{ bio }}</p>
           </div>
      </div>
+     </RouterLink>
 </template>
--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1,3 +1,8 @@
+<script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+</script>
+
 <template>
-     Card Details Page
+    {{ route.params.id }}
 </template>
