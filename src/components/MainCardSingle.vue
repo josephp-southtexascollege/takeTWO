6edc2adff5e63df6831ab8326902ba9827537f95
josephperez956 <josephperez@yahoo.com>
2025-11-18 22:15:43 -0600

    created card components

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { faker } from '@faker-js/faker';
+const firstname = faker.person.firstName();
+const lastname = faker.person.lastName();
+
+
+</script>
+
+<template>
+     {{ firstname }} {{ lastname  }} <br/>
+
+
+</template>
