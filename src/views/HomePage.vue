48bcca9e29fc600273a71abf8b27de9bac933109
josephperez956 <josephperez@yahoo.com>
2025-11-19 02:11:17 -0600

    pagination working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,10 @@
 <script setup>
 import MainCards from '@/components/MainCards.vue'
-
+import MainPagination from '@/components/MainPagination.vue';
 </script>
 
 <template>
 
      <MainCards></MainCards>
-
+     <MainPagination></MainPagination>
 </template>
