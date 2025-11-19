040159217891edde1163cea25fc20f9f49e948a0
josephperez956 <josephperez@yahoo.com>
2025-11-18 22:56:18 -0600

    fixed top rounded corners

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -2,12 +2,18 @@
 import { faker } from '@faker-js/faker';
 const firstname = faker.person.firstName();
 const lastname = faker.person.lastName();
-
-
+const jobtitle = faker.person.jobTitle();
+const bio = faker.lorem.lines(2);
+const username = faker.internet.username({ firstName: firstname, lastName: lastname }).toLowerCase();
 </script>
 
 <template>
-     {{ firstname }} {{ lastname  }} <br/>
-
-
+     <div class="border border-gray-400 rounded-t-xl shadow bg-gray-200 h-full">
+          <img class="object-fill h-480 w-960 rounded-t-xl" v-bind:src="faker.image.urlLoremFlickr({category: 'cars', height: 480, width: 960})" />
+          <div class="p-4">
+               <p class="mb-2 text-2xl font-bold ">{{ firstname }} {{ lastname  }} </p>
+               <p class="mb-3 font-normal text-gray-700 ">{{ jobtitle }}</p>
+               <p class="mb-3 font-nornaml text-gray-700 ">{{ bio }}</p>
+          </div>
+     </div>
 </template>
